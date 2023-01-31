6c94464a974c381a482efcd976ae19bffc2cac76
sheroanh <anhnguyentuan8@gmail.com>
2023-02-01 01:13:21 +0700

    first commit'

---
+++ anhit-fe/src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return(
+        <div className="h-screen w-screen bg-gray-100 text-gray-700">
+            <div className="grid grid-auto-rows content-center justify-items-center h-full w-full">
+                <img className="w-20" src="/logo.svg" alt="logo"/>
+                <p className="text-lg md:text-xl lg:text-3xl text-center font-semibold">Maintenance</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ anhit-fe/src/pages/maintenance/index.jsx
@@ -0,0 +1,13 @@
+const Maintenance = () => {
+    return(
+        <div className="h-screen w-screen bg-gray-100 text-gray-700">
+            <div className="grid grid-auto-rows content-center justify-items-center h-full w-full">
+                <img className="w-20 md:w-32" src="/logo.svg" alt="logo"/>
+                <p className="text-lg md:text-xl lg:text-3xl text-center font-semibold">Maintenance</p>
+                <p className="text-sm text-center italic">See you soon!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Maintenance;
